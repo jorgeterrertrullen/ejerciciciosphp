479a16736a1352dfb099409ebfc50b436e1a7e98
jorge <jorget023@gmail.com>
2020-09-25 15:50:28 +0100

    ejercicio7

---
+++ ejercicios/07.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $paises = array("españa", "francia", "italia" ,"portugal");
+        echo "<pre>";
+        echo print_r($paises);
+        echo "</pre>";
+    ?>
+</body>
+</html>
